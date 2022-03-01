9666a6c72b2aa8d4b1b3e203ca4b88de04180040
Scott Everett <burningcupidxxx@gmail.com>
2022-02-28 21:12:29 -0800

    Complete functions to CRUD to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,19 +3,52 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
+      // Add database schema if not already initialized
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // Create new object store for data and give it 'id' keyname, which increments automatically
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// PUT to database
+export const putDb = async (content) => {
+  console.error('PUT to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tw.objectStore('jate');
+  const request = store.put(content);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+
+};
+
+// Get all conent from database
+export const getDb = async () => {
+  console.log('GET all from database');
+
+  // create connection to database and version we want to use
+  const jateDb = await openDB('jate', 1);
+
+  // create new transaction and specify the database and data privileges
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // open desired object store
+  const store = tx.objectStore('jate');
+
+  // get all data in database
+  const request = store.getAll();
+
+  // get confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
 
 initdb();
