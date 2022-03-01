6571c363e3111eb47fbc5f33536e45a29c926d9a
Scott Everett <burningcupidxxx@gmail.com>
2022-02-28 22:39:30 -0800

    add optional chaining to getDb function to return value of result if present

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,16 +15,15 @@ const initdb = async () =>
   });
 
 // PUT to database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log('PUT to the database');
 
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tw.objectStore('jate');
-  const request = store.put({ id: id, content: content });
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('result.value', result);
-  return result;
 
 };
 
@@ -47,7 +46,7 @@ export const getDb = async () => {
   // get confirmation of the request
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 
