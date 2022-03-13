6bcafc499427c5335abca8d026b83aae34af1318
Scott Everett <burningcupidxxx@gmail.com>
2022-03-13 14:01:57 -0700

    Fix link to demonstration gif

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -36,7 +34,7 @@ module.exports = () => {
             inject: true,
             name: 'Just Another Text Editor',
             short_name: 'JATE',
-            description: 'Never forget your contacts!',
+            description: 'It\'s another text editor!',
             background_color: '#225ca3',
             theme_color: '#225ca3',
             start_url: '/',
